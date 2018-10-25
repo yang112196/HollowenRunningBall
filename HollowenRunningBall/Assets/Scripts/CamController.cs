8d99f404d2861b3ec6ba660b696b6ff10ccde763
yang112196 <yang112196@gmail.com>
2018-10-24 23:54:07 -0500

    pickups and ramp

--- HollowenRunningBall/Assets/Scripts/CamController.cs
+++ HollowenRunningBall/Assets/Scripts/CamController.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CamController : MonoBehaviour {
-    public Rigidbody rb;
-
+    //public Rigidbody rb;
+    public GameObject player;
+    private Vector3 offset;
 	// Use this for initialization
 	void Start () {
-        rb = GetComponent<Rigidbody>();
+        //rb = GetComponent<Rigidbody>();
+        offset = transform.position - player.transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        rb.velocity = new Vector3(0,0,4);
-	}
+        //rb.velocity = new Vector3(0,0,4);
+        this.transform.position = player.transform.position + offset;
+    }
 }
--- HollowenRunningBall/Assets/Scripts/PlayerController.cs
+++ HollowenRunningBall/Assets/Scripts/PlayerController.cs
@@ -2,18 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
     public Rigidbody rb;
     public float horizVel;
     public KeyCode moveL;
     public KeyCode moveR;
     public bool isLocked;
     public int laneNum;
+    //float yMovement;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
 
-        rb = GetComponent < Rigidbody > ();
+        rb = GetComponent<Rigidbody>();
 
         horizVel = 0;
 
@@ -21,29 +24,45 @@ public class PlayerController : MonoBehaviour {
 
         isLocked = false;
 
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    }
 
+    // Update is called once per frame
+    void Update()
+    {
+        //rb.AddForce(new Vector3(horizVel * 3, yMovement, 4));
         rb.velocity = new Vector3(horizVel, 0, 4);
-        if (Input.GetKeyDown(moveL) && (laneNum > 1) && !isLocked){
+        if (Input.GetKeyDown(moveL) && (laneNum > 1) && !isLocked)
+        {
             horizVel = -2;
-            StartCoroutine(stopSlide());
+            StartCoroutine(stopSliding());
             laneNum--;
             isLocked = true;
         }
         if (Input.GetKeyDown(moveR) && (laneNum < 3) && !isLocked)
         {
             horizVel = 2;
-            StartCoroutine(stopSlide());
+            StartCoroutine(stopSliding());
             laneNum++;
             isLocked = true;
         }
 
     }
 
-    IEnumerator stopSlide()
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+        }
+        if (other.gameObject.CompareTag("Fall Down"))
+        {
+            Destroy(gameObject);
+        }
+
+
+    }
+
+    IEnumerator stopSliding()
     {
         yield return new WaitForSeconds(0.5f);
         horizVel = 0;
